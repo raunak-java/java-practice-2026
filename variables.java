622b0b1ad0887592825b29898264b8217e4711a1
raunak-java <raunakkashyap8089@gmai.com>
2026-07-23 15:43:51 +0530

    Add variables program

---
+++ variables.java
@@ -0,0 +1,13 @@
+public class variables {
+    public static void main(String[] args) {
+      int a=10;
+      int b=5;
+      String name = "Tony";
+      System.out.println(a);
+      System.out.println(b);
+      System.out.println(name);  
+
+      a=50;
+      System.out.println(b=a);
+    }
+}
