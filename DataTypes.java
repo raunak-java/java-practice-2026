673b6a62eb857c2fddc1ce3e77c0a6a123ae9c30
raunak-java <raunakkashyap8089@gmai.com>
2026-07-25 13:52:36 +0530

    Added java Datatype example

---
+++ DataTypes.java
@@ -0,0 +1,22 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        byte b = 8;
+        System.out.println("Byte: " + b);
+
+        char ch = 'a';
+        System.out.println("Character: " + ch);
+
+        boolean var = false;
+        System.out.println("Boolean: " + var);
+
+        float f = 10.5f;
+        System.out.println("Float: " + f);
+
+        long a = 99999999L;
+        System.out.println("Long: " + a);
+
+        short n = 320;
+        System.out.println("Short: " + n);
+    }
+}
