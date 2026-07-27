7ec735ca52f9d46f13bad55ca295b61c1878fd10
raunak-java <raunakkashyap8089@gmai.com>
2026-07-27 12:38:56 +0530

    Added ternary operator example

---
+++ Ternary.java
@@ -0,0 +1,7 @@
+public class Ternary {
+    public static void main(String[] args) {
+        int num=87;
+        String type = ((num%2)==0) ? "even":"odd";
+        System.out.println(type);
+    }
+}
